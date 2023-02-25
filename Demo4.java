9ba3596dc35a81e890e22d928da2080d4f8600ab
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2023-02-25 20:21:18 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,15 @@
+package Program;
+
+public class Demo4 {
+
+	public static void main(String[] args) 
+	{
+	    Demo1 d1 =new Demo1();
+	    Demo2 d2 =new Demo2();
+	    Demo3 d3 =new Demo3();
+	    d1.start();
+	    d2.start();
+	    d3.start();
+	}
+
+}
