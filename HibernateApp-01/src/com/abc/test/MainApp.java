f3a541f6974dcfa9e2756d7230d84871293aad64
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2023-03-17 22:55:36 +0530

    Add files via upload

---
+++ HibernateApp-01/src/com/abc/test/MainApp.java
@@ -0,0 +1,37 @@
+package com.abc.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.abc.model.Student;
+
+public class MainApp {
+
+	public static void main(String[] args)
+	{
+	  Configuration conf = new Configuration();
+	  conf.configure("hibernate.cfg.xml");
+	  
+	  SessionFactory factory=conf.buildSessionFactory();
+	  System.out.println("SessionFactory implementation class name is ::" + factory.getClass().getName());
+	  
+	  Session session=factory.openSession();
+	  System.out.println("Session implementation class name is ::" + session.getClass().getName());
+	  
+	  Transaction transaction = session.beginTransaction();
+	  
+	  Student student= new Student();
+	  student.setSid(18);
+	  student.setSname("kohli");
+	  student.setSaddress("RCB");
+	  
+	  session.save(student);
+	  transaction.commit();
+	  
+	  session.close();
+	  factory.close();
+	}
+
+}
